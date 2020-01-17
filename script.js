99a91f9d5f168b5d28e0e3f1fe7425edf57392e6
bek <Umrbek@mail.ru>
2020-01-17 11:43:54 +0500

    dzjs1

---
+++ script.js
@@ -0,0 +1,17 @@
+var name= prompt("введите своё имя");
+console.log(" Имя ползователья-- " + name);
+var vozrast=prompt("Ваш возраст");
+console.log(" Возраст ползователя-- " + vozrast);
+var pi=prompt(" 16 + 3 = ");
+console.log(" 16 + 3= 19 " + " Ответь ползователя был-- " + pi);
+var minus=prompt(" 16 - 3 = ");
+console.log(" 16 - 3 = 13 " + " Ответь ползователя был-- " + minus);
+var um=prompt(" 16 * 3 = ");
+console.log(" 16 * 3 = 48 " + " Ответь ползователя был-- " + um);
+var deleniya=prompt(" 16 / 3 = ");
+console.log(" 16 / 3= 5.33 " + " Ответь ползователя был-- " + deleniya);
+var ostatka=prompt(" 16 % 3 = ");
+console.log(" 16 % 3= 1 " + " Ответь ползователя был-- " + ostatka);
+
+alert("Открыть консоль");
+
